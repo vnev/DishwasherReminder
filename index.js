b100ba9a98cb1a2e0517ec27d0c30bd640c06a18
Vedant Nevetia <nevetia.vedant@gmail.com>
2017-10-02 18:30:45 -0400

    update index.js. probably complete

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 var config = require("./config.json");
 var twilio = require("twilio");
-var app = require("express")();
-
 
 var client = new twilio(config["account_id"], config["auth_token"]);
 
+var person_one = config["to_numbers"][Math.floor(Math.random() * config["to_numbers"].length)];
+var person_two = config["to_numbers"][Math.floor(Math.random() * config["to_numbers"].length)];
+
+// make sure the same person is not sent the same message twice
+while (person_one === person_two) {
+	person_one = config["to_numbers"][Math.floor(Math.random() * config["to_numbers"].length)];
+}
+
+// send message to person one
 client.messages.create({
-	body: "Hello from node server",
-	to: config["to_numbers"][0],
+	body: "Its your turn for dishwasher duty boi",
+	to: person_one,
+	from: config["from_number"]
+}).then((message) => console.log(message.sid));
+
+// send message to person two
+client.messages.create({
+	body: "Its your turn for dishwasher duty boi",
+	to: person_two,
 	from: config["from_number"]
 }).then((message) => console.log(message.sid));
